6175521a56de1f2cecc7eb975ffce01e6ace028a
Quyen0308 <quyenbtvce160821@fpt.edu.vn>
2023-11-07 21:01:12 +0700

    update cmt RegisterActivity

--- app/src/main/java/com/example/se1606_prm392_group05/View/RegisterActivity.java
+++ app/src/main/java/com/example/se1606_prm392_group05/View/RegisterActivity.java
@@ -19,9 +19,11 @@ import java.util.ArrayList;
 
 public class RegisterActivity extends AppCompatActivity {
     private ArrayList<User> userList;
+    // Khai báo các EditText để nhập thông tin đăng ký, button đăng ký và button quay lại màn hình đăng nhập.
     private EditText etUsername, etPassword, etPasswordConfirm, etEmail, etPhoneNum, etAddress;
     private Button btnRegister;
 
+    // Khởi tạo RegisterHelper để truy cập database.
     private RegisterHelper databaseHelper;
 
     @Override
@@ -66,13 +68,13 @@ public class RegisterActivity extends AppCompatActivity {
         String address = etAddress.getText().toString().trim();
 
         if (username.isEmpty() || password.isEmpty() || passwordConfirm.isEmpty() || email.isEmpty() || phoneNum.isEmpty() || address.isEmpty()) {
-            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show(); // Báo lỗi nếu trống
         } else if (!password.equals(passwordConfirm)) {
             Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
         } else if (!email.matches(".*@gmail\\.com") || email.length() > 20) {
-            Toast.makeText(this, "Please enter a valid Gmail address (max 20 characters)", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please enter a valid Gmail address (max 20 characters)", Toast.LENGTH_SHORT).show(); // Báo lỗi nếu mật khẩu không khớp
         } else if (phoneNum.length() != 10) {
-            Toast.makeText(this, "Please enter a 10-digit phone number", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please enter a 10-digit phone number", Toast.LENGTH_SHORT).show(); // Kiểm tra định dạng email, số điện thoại
         } else if (!phoneNum.matches("\\d+")) {
             Toast.makeText(this, "Please enter a numeric phone number", Toast.LENGTH_SHORT).show();
         } else {
@@ -82,29 +84,31 @@ public class RegisterActivity extends AppCompatActivity {
             // Thêm người dùng vào cơ sở dữ liệu
             boolean isSuccess = addUserToDatabase(newUser);
             if (isSuccess) {
-                Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show(); // Hiển thị thông báo thành công
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class); // Mở LoginActivity
                 startActivity(intent);
                 finish();
             } else {
-                Toast.makeText(this, "Failed to register", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Failed to register", Toast.LENGTH_SHORT).show(); // Hiển thị thông báo lỗi
             }
         }
     }
 
+    // Gọi phương thức addUserToDatabase() để thêm vào database
     private boolean addUserToDatabase(User user) {
-        SQLiteDatabase db = databaseHelper.getWritableDatabase();
-
+        SQLiteDatabase db = databaseHelper.getWritableDatabase(); // Lấy writable database
+        // Khởi tạo ContentValues để chứa dữ liệu người dùng
         ContentValues values = new ContentValues();
+        // Insert vào bảng user
         values.put(RegisterHelper.COLUMN_USERNAME, user.getUsername());
         values.put(RegisterHelper.COLUMN_PASSWORD, user.getPassword());
         values.put(RegisterHelper.COLUMN_EMAIL, user.getEmail());
         values.put(RegisterHelper.COLUMN_PHONE, user.getPhone());
         values.put(RegisterHelper.COLUMN_ADDRESS, user.getAddress());
-
+        // Đóng database
         long result = db.insert(RegisterHelper.TABLE_USER, null, values);
         db.close();
-
+        // Trả về kết quả thành công/thất bại
         return result != -1;
     }
 
