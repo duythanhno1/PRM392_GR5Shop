091e74eedb1ce66af029e6b35a8c05079de425a2
datle202 <ledat8875@gmail.com>
2023-11-08 00:21:10 +0700

    up reset pass

--- app/src/main/java/com/example/se1606_prm392_group05/View/ResetPasswordActivity.java
+++ app/src/main/java/com/example/se1606_prm392_group05/View/ResetPasswordActivity.java
@@ -23,32 +23,38 @@ public class ResetPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
-
+// Khởi tạo các thành phần giao diện
         username = findViewById(R.id.username_reset_text);
         pass = findViewById(R.id.etPasswordReset);
         passConfirm = findViewById(R.id.etPasswordResetConfirm);
         resetPassword = findViewById(R.id.btnConfirmResetPassword);
         DB = new RegisterHelper(this);
-
+// Lấy dữ liệu từ Intent để hiển thị tên người dùng cần đặt lại mật khẩu
         Intent intent = getIntent();
         username.setText(intent.getStringExtra("username"));
-
+// Đặt sự kiện khi nút "Xác nhận đặt lại mật khẩu" được click
         resetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 String user = username.getText().toString();
                 String password = pass.getText().toString();
                 String rePassword = passConfirm.getText().toString();
+                // Kiểm tra tính hợp lệ của mật khẩu và xác nhận mật khẩu
                 if(password.equals(rePassword)){
+                    // Cập nhật mật khẩu trong cơ sở dữ liệu
                     Boolean checkPasswordUpdate = DB.updatePassword(user, password);
                     if(checkPasswordUpdate == true){
+                        // Chuyển đến màn hình đăng nhập và hiển thị thông báo thành công
                         Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                         startActivity(intent);
                         Toast.makeText(ResetPasswordActivity.this, "Password updated successfully", Toast.LENGTH_SHORT).show();
                     } else {
+                        // Hiển thị thông báo khi cập nhật mật khẩu không thành công
                         Toast.makeText(ResetPasswordActivity.this, "Password updated unsuccessfully", Toast.LENGTH_SHORT).show();
                     }
                 } else {
+                    // Hiển thị thông báo khi mật khẩu và xác nhận mật khẩu không khớp
                     Toast.makeText(ResetPasswordActivity.this, "Password isn't matching", Toast.LENGTH_SHORT).show();
                 }
             }
